8e9dbc090983681ca94a09ea7aaa3a4722c515f9
Tom Heslop <t_heslop@hotmail.co.uk>
2014-11-06 19:13:41 +0000

    initial commit, run yo angular, uncheck all options, then npm install

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gulfstreamApp
+ * @description
+ * # gulfstreamApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gulfstreamApp', []);
