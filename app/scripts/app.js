671ac94fba551d43e6ac99e804c9c5d797256bbf
Tom Heslop <t_heslop@hotmail.co.uk>
2014-11-06 22:48:23 +0000

    bower install ui-router + ngResource, grunt wiredep. Create states for home and about

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,4 +12,18 @@ angular
   .module('gulfstreamApp', [
     'ui.router',
     'ngResource'
-  ]);
+  ])
+  .config(function($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/home');
+
+      $stateProvider
+          .state('home', {
+              url: '/home',
+              templateUrl: 'views/main.html',
+              controller: 'MainCtrl'
+          })
+          .state('about', {
+              url: '/about',
+              templateUrl: 'views/about.html',
+          });
+  });
