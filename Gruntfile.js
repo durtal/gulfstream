f5247cba482e2c6262651a6efa53c21d19994349
Tom Heslop <t_heslop@hotmail.co.uk>
2015-02-06 14:29:20 +0000

    grunt buildcontrol task added to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -351,6 +351,21 @@ module.exports = function (grunt) {
     }
   });
 
+  // grunt build control
+  buildcontrol: {
+      options: {
+          dir: 'dist',
+          commit: true,
+          push: true
+      },
+      pages: {
+          options: {
+              remote: 'git@github.com:durtal/gulfstream.git',
+              branch: 'gh-pages'
+          }
+      }
+  }
+
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
