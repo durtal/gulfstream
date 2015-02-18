79970abf32eb98dd896d15b1d0b98ebae0364db9
Tom Heslop <t_heslop@hotmail.co.uk>
2015-02-18 21:36:48 +0000

    yo angular:factory d3 -means d3 can be injected into controllers and directives, rather than call .d3

---
+++ app/scripts/services/d3.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name gulfstreamApp.d3
+ * @description
+ * # d3
+ * Factory in the gulfstreamApp.
+ */
+angular.module('gulfstreamApp')
+  .factory('d3', function () {
+    // Service logic
+    // ...
+
+    return window.d3;
+  });
