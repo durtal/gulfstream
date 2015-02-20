f684625fdfa745b0ff64f8cf22adc19869d0a3e0
Tom Heslop <t_heslop@hotmail.co.uk>
2015-02-20 00:20:03 +0000

    finished zipf_hcp vis blog

--- app/scripts/controllers/zipf_hcpvis.js
+++ app/scripts/controllers/zipf_hcpvis.js
@@ -8,36 +8,36 @@
  * Controller of the gulfstreamApp
  */
 angular.module('gulfstreamApp')
-  .controller('ZipfHcpvisCtrl', function ($scope, _, $http, $window) {
+    .controller('ZipfHcpvisCtrl', function ($scope, _, $http, d3) {
 
-      $scope.ExampleRaces = [
-        {
-            'year': 2009,
-            'p1' : 100,
-            'p2' : 98,
-            'p3' : 95,
-            'p4' : 90,
-            'p5' : 76
-        },
-        {
-            'year': 2010,
-            'p1' : 95,
-            'p2' : 94,
-            'p3' : 93,
-            'p4' : 88,
-            'p5' : 87
-        },
-        {
-            'year': 2011,
-            'p1' : 102,
-            'p2' : 90,
-            'p3' : 80,
-            'p4' : 75,
-            'p5' : 73
-        }
-      ];
+        $scope.ExampleRaces = [
+            {
+                'year': 2009,
+                'p1' : 100,
+                'p2' : 98,
+                'p3' : 95,
+                'p4' : 90,
+                'p5' : 76
+            },
+            {
+                'year': 2010,
+                'p1' : 95,
+                'p2' : 94,
+                'p3' : 93,
+                'p4' : 88,
+                'p5' : 87
+            },
+            {
+                'year': 2011,
+                'p1' : 102,
+                'p2' : 90,
+                'p3' : 80,
+                'p4' : 75,
+                'p5' : 73
+            }
+        ];
 
-      $scope.weightedRatings = [
+        $scope.weightedRatings = [
         {
             'year' : 2009,
             'p1' : 100,
@@ -64,123 +64,89 @@ angular.module('gulfstreamApp')
         }
       ];
 
-      $scope.unratedRace = {
+        $scope.unratedRace = {
           'year': 2012,
           'p1': 0,
           'p2': 5,
           'p3': 10,
           'p4': 15,
           'p5': 20
-      };
+        };
 
-      $scope.singleRace = {
-          'p1' : 0,
-          'p2' : 5,
-          'p3' : 10,
-          'p4' : 15,
-          'p5' : 20
-      };
+        $scope.unratedRace2 = {
+            'year': 2012,
+            'p1': 0,
+            'p2': 5,
+            'p3': 10,
+            'p4': 15,
+            'p5': 20
+        };
 
-/*  CODE FOR ZIPF RACE
-      var x = [100, 	98, 	95, 	90, 	76];
-      var y = [0, 5, 10, 15, 20];
+        $scope.singleRace = {
+          'p1' : 100,
+          'p2' : 95,
+          'p3' : 90,
+          'p4' : 85,
+          'p5' : 80
+        };
 
-      var z = [];
+        function zipfRace(diff, pastRace) {
+            var wgtRtgs = {
+                'p1': pastRace.p1 + parseInt((diff.p1)),
+                'p2': pastRace.p2 + parseInt((diff.p2)),
+                'p3': pastRace.p3 + parseInt((diff.p3)),
+                'p4': pastRace.p4 + parseInt((diff.p4)),
+                'p5': pastRace.p5 + parseInt((diff.p5))
+            };
+            wgtRtgs = {
+                'p1': wgtRtgs.p1 * 1/1,
+                'p2': wgtRtgs.p2 * 1/2,
+                'p3': wgtRtgs.p3 * 1/3,
+                'p4': wgtRtgs.p4 * 1/4,
+                'p5': wgtRtgs.p5 * 1/5
+            };
+            var total = wgtRtgs.p1 + wgtRtgs.p2 + wgtRtgs.p3 + wgtRtgs.p4 + wgtRtgs.p5;
+            var zipfs = 1/1 + 1/2 + 1/3 + 1/4 + 1/5;
+            var rating = total / zipfs;
 
-      for(var i = 0; i < x.length; i++) {
-          z[i] = parseInt(x[i]) + parseInt(y[i]);
-      }
-      console.log(z);
+            return rating;
+        }
+        $scope.unratedRaceRating = zipfRace($scope.unratedRace2, $scope.ExampleRaces[0]);
 
-      var winning = [];
-      for(var t = 0; t < z.length; t++) {
-          var zipf = 1 / (t + 1);
-          winning[t] = parseInt(z[t]) * zipf;
-      }
+        $scope.$watch('unratedRace2', function(data) {
+            $scope.unratedRaceRating = zipfRace(data, $scope.ExampleRaces[0]);
+        }, true);
 
-      console.log(winning);
+        $http.get('data/dummy-ratings.json').success(function(ratingsdata) {
+            $scope.dummyRatings = ratingsdata;
+            $scope.dummyWinningRatings = _.map($scope.dummyRatings, function(x) {
+                return x.p1;
+            });
 
-      var total = _.reduce(winning, function(memo, num) {
-          return memo + num;
-      }, 0);
-      console.log(total);
+            $scope.zipfHcpRatings = [];
+            _.each($scope.dummyRatings, function(element, index) {
+                var y = zipfRace($scope.unratedRace2, element);
+                $scope.zipfHcpRatings[index] = y;
+            });
+            $scope.avgRtg = d3.mean($scope.zipfHcpRatings);
 
-      var zipfs = [];
 
-      for(var p = 0; p < winning.length; p++) {
-          zipfs[p] = 1 / (p+1);
-      }
-      console.log(zipfs);
+            $scope.unratedRace3 = {
+                'p1': 0,
+                'p2': 5,
+                'p3': 10,
+                'p4': 15,
+                'p5': 20
+            };
 
-      var zipfTotal = _.reduce(zipfs, function(memo, num) {
-          return memo + num;
-      }, 0);
-      console.log(zipfTotal);
+            $scope.$watch('unratedRace3', function(data) {
+                var race = data;
+                _.each($scope.dummyRatings, function(element, index) {
+                    var y = zipfRace(race, element);
+                    $scope.zipfHcpRatings[index] = y;
+                });
+                $scope.avgRtg = d3.mean($scope.zipfHcpRatings);
+            }, true);
 
-      var rating = total / zipfTotal;
-      console.log(rating);
-*/
-
-      $http.get('data/dummy-ratings.json').success(function(ratingsdata) {
-          $scope.dummyRatings = ratingsdata;
-          $scope.dummyWinningRatings = _.map($scope.dummyRatings, function(x) {
-              return x.p1;
-          });
-          console.log($scope.dummyWinningRatings);
-
-          // $window.d3 code for a histogram - taken from http://bl.ocks.org/mbostock/3048450
-          var values = $window.d3.range(1000).map($window.d3.random.bates(2));
-
-          var margin = {top: 10, right: 30, bottom: 30, left: 30},
-          width = 550 - margin.left - margin.right,
-          height = 340 - margin.top - margin.bottom;
-
-          var x = $window.d3.scale.linear()
-          .domain([0, 1])
-          .range([0, width]);
-
-          // Generate a histogram using twenty uniformly-spaced bins.
-          var data = $window.d3.layout.histogram()
-          .bins(x.ticks(20))
-          (values);
-
-          var y = $window.d3.scale.linear()
-          .domain([0, $window.d3.max(data, function(d) { return d.y; })])
-          .range([height, 0]);
-
-          var xAxis = $window.d3.svg.axis()
-          .scale(x)
-          .orient('bottom');
-
-          var svg = $window.d3.select('#test-histogram').append('svg')
-          .attr('width', width + margin.left + margin.right)
-          .attr('height', height + margin.top + margin.bottom)
-          .append('g')
-          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-          var bar = svg.selectAll('.bar')
-          .data(data)
-          .enter().append('g')
-          .attr('class', 'bar')
-          .attr('transform', function(d) { return 'translate(' + x(d.x) + ',' + y(d.y) + ')'; });
-
-          bar.append('rect')
-          .attr('x', 1)
-          .attr('width', x(data[0].dx) - 1)
-          .attr('height', function(d) { return height - y(d.y); });
-
-          bar.append('text')
-          .attr('dy', '.75em')
-          .attr('y', 6)
-          .attr('x', x(data[0].dx) / 2)
-          .attr('text-anchor', 'middle');
-
-          svg.append('g')
-          .attr('class', 'x axis')
-          .attr('transform', 'translate(0,' + height + ')')
-          .call(xAxis);
-
-      });
-
-      console.log($scope.dummyWinningRatings);
+        });
   });
