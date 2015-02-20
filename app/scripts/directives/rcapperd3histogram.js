c4b36d25c36dc6bcaaff3ed5da235a21e91af35a
Tom Heslop <t_heslop@hotmail.co.uk>
2015-02-20 00:19:14 +0000

    rcapper-d3-histogram directive

--- app/scripts/directives/rcapperd3histogram.js
+++ app/scripts/directives/rcapperd3histogram.js
@@ -7,12 +7,92 @@
  * # rcapperD3Histogram
  */
 angular.module('gulfstreamApp')
-  .directive('rcapperD3Histogram', function () {
+  .directive('rcapperD3Histogram', function (d3) {
+
+    function link(scope, element, attr) {
+
+        // svg dimensions
+        var margin = {
+                top: 10,
+                right: 10,
+                bottom: 40,
+                left: 10
+            },
+            width = 550 - margin.left - margin.right,
+            height = 340 - margin.top - margin.bottom;
+
+        var svg = d3.select(element[0])
+            .append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom);
+
+        scope.$watch('data', function(data) {
+
+            svg.selectAll('*').remove();
+
+            var xmin = Math.floor(d3.min(data)), // watch
+                xmax = Math.ceil(d3.max(data));
+
+            var bins = xmax - xmin; // watch
+
+            var x = d3.scale.linear() // watch
+                .domain([0, bins])
+                .range([0, width]);
+
+            var x2 = d3.scale.linear() // watch
+                .domain([xmin, xmax])
+                .range([0, width]);
+
+            var hist = d3.layout.histogram() // watch
+                .bins(bins)(data);
+                console.log(hist);
+
+            var y = d3.scale.linear() // watch
+                .domain([0, d3.max(hist, function(d) {
+                    return d.y;
+                })])
+                .range([height, 0]);
+
+            var xAxis = d3.svg.axis() // watch
+                .scale(x2)
+                .orient('bottom');
+
+            svg.append('g')
+                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+            var bar = svg.selectAll('.bar') // watch
+                .data(hist)
+                .enter()
+                .append('g')
+                .attr('class', 'bar')
+                .attr('transform', function(d) {
+                    return 'translate(' + x2(Math.round(d.x)) + ',' + y(d.y) + ')';
+                })
+                .style('fill', attr.fill);
+
+            bar.append('rect') // watch
+                .attr('x', 1)
+                .attr('width', x(hist[0].dx))
+                .attr('height', function(d) {
+                    return height - y(d.y);
+                })
+                .style('opacity', attr.opacity);
+
+            svg.append('g') // watch
+                .attr('class', 'x-axis')
+                .attr('transform', 'translate(0,' + height + ')')
+                .call(xAxis);
+
+        }, true);
+
+
+    }
     return {
-      template: '<div></div>',
+    //   template: '<div></div>',
       restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the rcapperD3Histogram directive');
-      }
+      scope: {
+          data: '='
+      },
+      link: link
     };
   });
