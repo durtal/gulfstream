1d42e300b3835cacffabee3261d29ed965de6b4c
Tom Heslop <t_heslop@hotmail.co.uk>
2015-04-24 21:41:45 +0100

    yo angular:controller date

---
+++ app/scripts/controllers/date.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gulfstreamApp.controller:DateCtrl
+ * @description
+ * # DateCtrl
+ * Controller of the gulfstreamApp
+ */
+angular.module('gulfstreamApp')
+  .controller('DateCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
