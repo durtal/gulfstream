ca660f0ff6488d7c4e75d3006ef0851280fe356c
Tom Heslop <t_heslop@hotmail.co.uk>
2015-04-24 22:52:06 +0100

    yo angular:controller race

---
+++ app/scripts/controllers/race.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gulfstreamApp.controller:RaceCtrl
+ * @description
+ * # RaceCtrl
+ * Controller of the gulfstreamApp
+ */
+angular.module('gulfstreamApp')
+  .controller('RaceCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
