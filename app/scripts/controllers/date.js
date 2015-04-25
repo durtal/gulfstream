76bdf5979a9e4575d99c37ee841c8c4df8bd9403
Tom Heslop <t_heslop@hotmail.co.uk>
2015-04-25 22:15:46 +0100

    update date controller

--- app/scripts/controllers/date.js
+++ app/scripts/controllers/date.js
@@ -12,20 +12,23 @@ angular.module('gulfstreamApp')
     .controller('DateCtrl', function ($scope, races, $stateParams, _) {
 
         $scope.date = $stateParams.raceDate;
-        $scope.races = races;
-        console.log(races);
-        $scope.predicate = 'race';
+        $scope.races = _.sortBy(races, 'race');
 
-        var rnrs = _.pluck(races, 'runners');
-        console.log(rnrs);
-
-        var wnrs = _.flatten(rnrs);
-        console.log(wnrs);
-
-        $scope.pos1 = _.where(wnrs, {
-            'pos': 1
-        });
-        console.log($scope.pos1);
+        $scope.raceId = _.pluck($scope.races, 'race');
+        console.log($scope.raceId);
+        // console.log(races);
+        // $scope.predicate = 'race';
+        //
+        // var rnrs = _.pluck(races, 'runners');
+        // console.log(rnrs);
+        //
+        // var wnrs = _.flatten(rnrs);
+        // console.log(wnrs);
+        //
+        // $scope.pos1 = _.where(wnrs, {
+        //     'pos': 1
+        // });
+        // console.log($scope.pos1);
 
         var chain = _(races).chain()
             .pluck('runners')
@@ -33,4 +36,14 @@ angular.module('gulfstreamApp')
             .where({ 'pos': 1 })
             .value();
         console.log(chain);
+
+        $scope.ratings = _(races).chain()
+            .pluck('runners')
+            .flatten()
+            .pluck('RRtg')
+            .filter(function(num) {
+                return num >= 0;
+            })
+            .value();
+
     });
