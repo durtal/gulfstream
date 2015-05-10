e7efe8df3ffb1cca3bf6800afcd9abe2cb271080
Tom Heslop <t_heslop@hotmail.co.uk>
2015-05-10 15:32:27 +0100

    fix calHeatmap directive

--- app/scripts/directives/calheatmap.js
+++ app/scripts/directives/calheatmap.js
@@ -10,33 +10,33 @@ angular.module('gulfstreamApp')
     .directive('calHeatmap', function() {
 
         function link(scope, element, attr) {
-            var config = scope.config || {},
-                element = element[0],
-                cal = new CalHeatMap()
-                defaults = {
-                    itemSelector: element,
-                    domain: 'month',
-                    subDomain: 'day',
-                    range: 3,
-                    cellSize: 14,
-                    legend: [10, 20, 30, 40, 50, 60, 70, 80, 90],
-                    legendColors: {
-                        min: '#D8D8D8',
-                        max: '#D9220F',
-                        empty: '#D8D8D8',
-                        base: '#FCFCFC'
-                    }
-                };
+            var config = scope.config || {};
+            var element = element[0];
+            var cal = new CalHeatMap();
+            var defaults = {
+                itemSelector: element,
+                domain: 'month',
+                subDomain: 'day',
+                range: 3,
+                cellSize: 14,
+                legend: [10, 20, 30, 40, 50, 60, 70, 80, 90],
+                legendColors: {
+                    min: '#D8D8D8',
+                    max: '#D9220F',
+                    empty: '#D8D8D8',
+                    base: '#FFFFFF'
+                }
+            };
             angular.extend(defaults, config);
             cal.init(defaults);
         }
 
         return {
-            template: '<div class="cal-heatmap" config="config"></div>'
+            template: '<div class="cal-heatmap" config="config"></div>',
             restrict: 'E',
             scope: {
                 config: '='
-            }
+            },
             link: link
         };
     });
