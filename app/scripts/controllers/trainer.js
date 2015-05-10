75ce7fd30fbcdeb09cf8faca727f170a9167e193
Tom Heslop <t_heslop@hotmail.co.uk>
2015-05-10 15:34:30 +0100

    yo angular:controller trainer

---
+++ app/scripts/controllers/trainer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gulfstreamApp.controller:TrainerCtrl
+ * @description
+ * # TrainerCtrl
+ * Controller of the gulfstreamApp
+ */
+angular.module('gulfstreamApp')
+  .controller('TrainerCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
